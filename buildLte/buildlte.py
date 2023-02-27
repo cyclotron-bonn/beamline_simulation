aabcbcc20d138ca04edb30bb23d001da778c2dfc
Leonhard Moske <leonhard.moske@gmail.com>
2023-02-27 13:43:37 +0100

    strinng assembly + clean

--- buildLte/buildlte.py
+++ buildLte/buildlte.py
@@ -1,9 +1,9 @@
 #TODO input toml files as command line argument
-#TODO refactor into functions
 #TODO dataclass TAG (for beta function) and clear
 #TODO string assembly for classes
 #TODO quads geometric strength
 #TODO manual geometric strength variation 
+#TODO documentation for functions
 
 # this script creates lte files and saves in ../lteFiles
 
@@ -43,42 +43,101 @@ class Drift:
 #-------------------------------------------------------------------------------
 
 # read lattice parameters into list of quads and bends
-quadsFile = open("../latticeParameter/latticeElements/c_weg_quads.toml") 
-quadDict = toml.load(quadsFile, _dict=dict)
+def readFileMagtoList(quadFile: str):
+    quadsFile = open(quadFile) #"../latticeParameter/latticeElements/c_weg_quads.toml" 
+    quadDict = toml.load(quadsFile, _dict=dict)
 
-quads = []
-bends = []
+    quads = []
+    bends = []
 
-for k, element in enumerate(quadDict):
-    if quadDict[element]["elegantKey"] == "quad":
-        quads.append(Quaddrupole(   element, "quad", 
-                                    quadDict[element]["l"], 
-                                    quadDict[element]["hA"], 
-                                    0 , # geometric strength maybe here maybe later
-                                    k)) 
-    elif quadDict[element]["elegantKey"] == "sbend":
-        bends.append(BendingMagnet( element, "sbend", 
-                                    quadDict[element]["l"], 
-                                    quadDict[element]["angle"], 
-                                    quadDict[element]["e1"],
-                                    quadDict[element]["e2"],
-                                    quadDict[element]["h1"],
-                                    quadDict[element]["h2"],
-                                    k))
+    for k, element in enumerate(quadDict):
+        if quadDict[element]["elegantKey"] == "quad":
+            quads.append(Quaddrupole(   element, "quad", 
+                                        quadDict[element]["l"], 
+                                        quadDict[element]["hA"], 
+                                        0 , # geometric strength maybe here maybe later
+                                        k)) 
+        elif quadDict[element]["elegantKey"] == "sbend":
+            bends.append(BendingMagnet( element, "sbend", 
+                                        quadDict[element]["l"], 
+                                        quadDict[element]["angle"], 
+                                        quadDict[element]["e1"],
+                                        quadDict[element]["e2"],
+                                        quadDict[element]["h1"],
+                                        quadDict[element]["h2"],
+                                        k))
 
-print(quads)
+    return quads, bends
 
-quadsFile = open("../latticeParameter/drifts/c_weg_drifts.toml") 
-driftDict = toml.load(quadsFile, _dict=dict)
+def readFileDrifttoList(driftFile):
+    quadsFile = open(driftFile) 
+    driftDict = toml.load(quadsFile, _dict=dict)
 
-drifts = []
-for k, element in enumerate(driftDict):
-    drifts.append(Drift(element, "drift", driftDict[element]["l"], k))
+    drifts = []
+    for k, element in enumerate(driftDict):
+        drifts.append(Drift(element, "drift", driftDict[element]["l"], k))
+
+    return drifts
+
+#-------------------------------------------------------------------------------
+
+def quadtoString(quad: Quaddrupole):
+    string = f"{quad.identifier}: {quad.elegantKey}, l={quad.l}, k1={quad.k}"
+    return string
+
+def bendtoString(bend: BendingMagnet):
+    string= f"{bend.identifier}: {bend.elegantKey}, l={bend.l}, angle={bend.angle}, e1={bend.e1}, e2={bend.e2}, h1={bend.h1}, h2={bend.h2}"
+    return string
+
+def drifttoString(drift: Drift):
+    string=f"{drift.identifier}: {drift.elegantKey}, l={drift.l}"
+    return string
+
+def clearstr(prevel, xlim, ylim):
+    return f"{prevel}CLEAN: clean,mode=\"absval\", xlimit={xlim}, ylimit={ylim}"
+
+def lineString(upperstring: str):
+    lines = upperstring.split("\n")
+    elements = []
+    for line in lines:
+        if line.split(":", 1)[0].strip() != "":
+            elements.append(line.split(":", 1)[0].strip())
+    string = "path: LINE=("
+    for e in elements:
+        string = string + e + ", "
+    string = string[:-2] + ")\n"
+    return string
 
 #-------------------------------------------------------------------------------
 
 
 
-def assembleLine(quads, bends, drifts, tags, clears = "all"):
+def assembleLine(quads, bends, drifts, tags, clears = "all", xlim= 0.3, ylim= 0.3):
     string = ""
+    for pos in range(len(drifts)):
+        string = string + drifttoString(drifts[pos]) + "\n"
+        if clears == "all":
+            string = string + clearstr(drifts[pos].identifier,xlim, ylim) + "\n"
+        for quad in quads:
+            if quad.position == pos:
+                string = string + quadtoString(quad) + "\n"
+                break
+        for bend in bends:
+            if bend.position == pos:
+                string = string + bendtoString(bend) + "\n"
+                break
+
+    string = string + lineString(string)
     return string
+
+def writelteFile(filename, magfile, driftfile):
+    quads, bends = readFileMagtoList(magfile)
+    drifts = readFileDrifttoList(driftfile)
+    string = assembleLine(quads, bends, drifts, 0)
+    with open(filename, "w") as text_file:
+        text_file.write(string)
+
+
+
+
+writelteFile("test.lte", "../latticeParameter/latticeElements/c_weg_quads.toml", "../latticeParameter/drifts/c_weg_drifts.toml")
